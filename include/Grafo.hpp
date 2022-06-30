6e0bc15fe147db3e19038eae98d40fb03d24df20
Lucas Agnez Lima <56660048+LucasAgnez@users.noreply.github.com>
2022-06-30 15:32:12 -0300

    Modularização

---
+++ include/Grafo.hpp
@@ -0,0 +1,45 @@
+#ifndef GRAFO_HPP
+#define GRAFO_HPP
+
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "Vertice.hpp"
+
+class Grafo {
+private:
+	int n;
+  int numero_cromatico;
+	std::vector<Vertice*> vertices;
+	std::vector<std::vector<Vertice*>> adjacentes;
+
+  public:
+
+	Grafo(std::string nome_arquivo);
+	
+  ~Grafo();
+
+  Vertice* max_saturacao(std::set<Vertice*> s);
+
+  void carrega_grafo(std::string nome_arquivo);
+
+	void adiciona_aresta(int u, int v);
+
+	void adiciona_vertice(Vertice *u);
+ 
+	void colore_grafo();
+	
+	void imprime_lista();
+	
+	void imprime_vertices();
+
+	void imprime_lista_colorida();
+	
+	bool eh_pseudografo();
+
+	void calcula_saturacao();
+};
+#endif
